6c03fd5141bef9f87aa60a1bffbc5d0ee1e52260
Richard Peterson <webbuyrich@gmail.com>
2015-04-10 10:32:54 -0500

    original commit up to second section that starts with the best viking gear

---
+++ index.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Viking App</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="author" href="humans.txt">
+    </head>
+    <body>
+        <div class="container">
+
+            <div class="wrapper">
+                
+                <!--START TOP NAV -->
+                <div class="top-nav">
+                    <div class="text-center">A Web Page</div>
+
+                    <div class="nav-elements">
+                        
+                        <div class="go-back glyphicon glyphicon-arrow-left"> </div>
+                        <div class="go-forward glyphicon glyphicon-arrow-right"></div>
+                        <div class="cancel glyphicon glyphicon-remove"></div>
+                        <div class="go-up glyphicon glyphicon-arrow-up"></div>
+                        <div class="url-bar">
+                            <input type="text" id="url-bar">
+                        </div>
+                        <div class="search-bar">
+                           <input type="text" id="search-bar" placeholder="Search">
+                            <img src="" alt="">
+                        </div>
+
+                    </div><!--end nav-elments-->
+                </div><!-- end top-nav--> 
+
+                <!--START NAV ELEMENTS -->
+                <div class="section-logo-cart">
+
+                    <div class="nav-elements">
+                        
+                        <div class="logo text-center">VIKING STORE</div>
+                        <div class="cart-text text-center">
+                            <div class="shopping-cart glyphicon glyphicon-envelope"></div>
+                            <div class="your-cart">Your Cart</div>
+                        </div>
+                        
+                    </div><!--end nav elements-->                  
+                </div><!-- end section logo cart--> 
+
+                <!--START SLIDESHOW -->
+                <div class="section-slideshow-main text-center">
+                    
+
+                    <h1 id="slideshow-text">The Viking Store</h1>
+
+                    
+                </div><!--end slideshow main-->
+
+
+                <!--START SLIDESHOW BOTTOM TEXT -->
+                <div class="section-slideshow-bottom text-center">
+
+                        <h2 id="slideshow-text-h2">The Best Viking Gear on the Whole Web</h2>
+                    
+                </div><!--end slideshow bottom-->
+
+            </div><!--end wrapper-->
+        </div><!--end container-->  
+        <script src="js/bootstrap.min.js"></script>     
+    </body>
+</html>
